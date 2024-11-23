fa0c502a6528c1b48591fae2dc78fab9eee47684
APSick <24arseniy12@gmail.com>
2024-11-23 22:03:47 +0300

    23/11

---
+++ module_1_6.py
@@ -0,0 +1,9 @@
+Completed_Dz = 12
+
+Hours_Spent = 1.5
+
+Name_Course = "Python"
+
+Time_Per_Task = Hours_Spent / Completed_Dz
+
+print("Курс:", Name_Course, "всего задач:", Completed_Dz, "затрачено часов:", Hours_Spent, "среднее время выполнения:", Time_Per_Task)
